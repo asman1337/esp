2e63f79238507542b734de6adca510c7ae125d26
Asman Mirza <rambo007.am@gmail.com>
2023-09-14 16:29:21 +0530

    added c++ module support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,11 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
@@ -46,6 +51,12 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
 }

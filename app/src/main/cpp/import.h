184f7264401fa4c586b239b8c6ba52243f97b4c1
Asman Mirza <rambo007.am@gmail.com>
2023-09-20 00:05:48 +0530

    added c++ module support

---
+++ app/src/main/cpp/import.h
@@ -0,0 +1,22 @@
+//
+// Created by asman on 19/09/23.
+//
+
+#ifndef ESP_IMPORT_H
+#define ESP_IMPORT_H
+#include <jni.h>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cerrno>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <android/log.h>
+#endif //ESP_IMPORT_H
